add01a39a8af86678be0530f24086b6e35928f8e
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-08-25 15:19:30 +0300

    add sign up functionality

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import { Switch, Route } from "react-router-dom";
 import ProjectList from "./components/projects/ProjectList";
 import Navbar from "./components/navbar/Navbar";
 import ProjectDetails from "./components/projects/ProjectDetails";
+import Signup from "./components/auth/Signup";
 
 class App extends React.Component {
+  state = {
+    isLoggedIn: false,
+    user: null,
+  };
+
+  getTheUser = (userObj, loggedIn) => {
+    this.setState({
+      user: userObj,
+      isLoggedIn: loggedIn,
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to our project management app</h1>
-        <Navbar />
+        <Navbar userData={this.state.user} userIsLoggedIn={this.state.isLoggedIn} />
         <Switch>
+          <Route exact path="/signup" render={(props) => <Signup {...props} getUser={this.getTheUser} />} />
           <Route exact path="/projects" component={ProjectList} />
           <Route exact path="/projects/:id" component={ProjectDetails} />
         </Switch>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar(props) {
   return (
     <nav className="nav-style">
       <ul>
+        <li>{props.userIsLoggedIn ? "Hello " + props.userData.username : "Please login"}</li>
         <li>
           <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/signup">Register</Link>
         </li>
       </ul>
     </nav>
